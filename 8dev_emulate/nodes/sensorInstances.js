b72891ae9c64787dc7085871d2530987689943e1
Tomas Markevičius <tomas.m@8devices.com>
2018-01-18 15:28:40 +0200

    Make resource handlers functions instead of object methods

--- 8dev_emulate/nodes/sensorInstances.js
+++ 8dev_emulate/nodes/sensorInstances.js
@@ -1,26 +1,29 @@
 'use strict';
 
-const crypto = require('crypto');
 const ClientNode = require('./clientNodeInstance.js');
 const { RESOURCE_TYPE } = require('./resourceInstance.js');
 
 function randomFloat(min, max) {
-  return Math.random() * (max - min) + min;
-};
+  return (Math.random() * (max - min)) + min;
+}
 
 function randomInteger(min, max) {
   return Math.floor(randomFloat(Math.ceil(min), Math.floor(max)));
-};
+}
+
+function powerSourceVoltageHandler(averageVoltage = 3300, fluctuation = 0.005) {
+  return randomInteger(averageVoltage * (1 - fluctuation), averageVoltage * (1 + fluctuation));
+}
+
+function temperatureSensorHandler(averageTemperature = 20, fluctuation = 0.025) {
+  return randomFloat(averageTemperature * (1 - fluctuation), averageTemperature * (1 + fluctuation));
+}
 
 class Sensor3700 extends ClientNode {
   constructor(lifetime, UUID, serverIP, clientPort) {
     super(lifetime, '8devices', '8dev_3700', true, UUID, serverIP, clientPort);
 
-    this.objects['/3/0'].addResource(7, 'R', RESOURCE_TYPE.INTEGER, 3300, this.powerSourceVoltageHandler);
-  }
-
-  powerSourceVoltageHandler() {
-    return randomInteger(3200, 3400);
+    this.objects['/3/0'].addResource(7, 'R', RESOURCE_TYPE.INTEGER, 3300, powerSourceVoltageHandler);
   }
 }
 
@@ -28,11 +31,7 @@ class Sensor3800 extends ClientNode {
   constructor(lifetime, UUID, serverIP, clientPort) {
     super(lifetime, '8devices', '8dev_3800', true, UUID, serverIP, clientPort);
 
-    this.objects['/3/0'].addResource(7, 'R', RESOURCE_TYPE.INTEGER, 3300, this.powerSourceVoltageHandler);
-  }
-
-  powerSourceVoltageHandler() {
-    return randomInteger(3200, 3400);
+    this.objects['/3/0'].addResource(7, 'R', RESOURCE_TYPE.INTEGER, 3300, powerSourceVoltageHandler);
   }
 }
 
@@ -42,26 +41,18 @@ class Sensor4400 extends ClientNode {
 
     this.createObject(3200, 0);
     this.createObject(3303, 0);
-
-    this.objects['/3/0'].addResource(7, 'R', RESOURCE_TYPE.INTEGER, 3300, this.powerSourceVoltageHandler);
+    
+    this.objects['/3/0'].addResource(7, 'R', RESOURCE_TYPE.INTEGER, 3300, powerSourceVoltageHandler);
     this.objects['/3200/0'].addResource(5500, 'R', RESOURCE_TYPE.BOOLEAN, false);
     this.objects['/3200/0'].addResource(5501, 'R', RESOURCE_TYPE.INTEGER, 0);
-    this.objects['/3303/0'].addResource(5700, 'R', RESOURCE_TYPE.FLOAT, 20.0, this.temperatureSensorHandler);
-  }
-
-  powerSourceVoltageHandler() {
-    return randomInteger(3200, 3400);
-  }
-
-  temperatureSensorHandler() {
-    return randomFloat(19.9, 20.1);
+    this.objects['/3303/0'].addResource(5700, 'R', RESOURCE_TYPE.FLOAT, 20.0, temperatureSensorHandler);
   }
 
   hallSensorTrigger() {
     const that = this;
-    this.objects['/3200/0'].getResourceValue(5500, function(value) {
+    this.objects['/3200/0'].getResourceValue(5500, (value) => {
       that.objects['/3200/0'].writeResource(5500, !value, true);
-      that.objects['/3200/0'].getResourceValue(5501, function(value){
+      that.objects['/3200/0'].getResourceValue(5501, (value) => {
         that.objects['/3200/0'].writeResource(5501, (value + 1) % (2 ** 31), true);
       });
     });
@@ -72,11 +63,7 @@ class Sensor4500 extends ClientNode {
   constructor(lifetime, UUID, serverIP, clientPort) {
     super(lifetime, '8devices', '8dev_4500', true, UUID, serverIP, clientPort);
 
-    this.objects['/3/0'].addResource(7, 'R', RESOURCE_TYPE.INTEGER, 3300, this.powerSourceVoltageHandler);
-  }
-
-  powerSourceVoltageHandler() {
-    return randomInteger(3200, 3400);
+    this.objects['/3/0'].addResource(7, 'R', RESOURCE_TYPE.INTEGER, 3300, powerSourceVoltageHandler);
   }
 }
 
