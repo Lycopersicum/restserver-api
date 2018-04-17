6a70f6d61c1195ab6fbf8a282ba7141d7f698a4c
Tomas Markevičius <tomas.m@8devices.com>
2018-04-17 13:30:18 +0300

    Fix bug of 2 byte identifier decryption fail

--- lwm2m/lwm2m.js
+++ lwm2m/lwm2m.js
@@ -311,7 +311,7 @@ function decodeTLV(buffer) {
   index += 1;
 
   i = index;
-  while (i <= (index + (buffer[0] >> 5) & 0b1)) { // eslint-disable-line no-bitwise
+  if (0 < (index + (buffer[0] >> 5) & 0b1)) { // eslint-disable-line no-bitwise
     if (buffer[i] === undefined) {
       throw Error('Given buffer is corrupted (missing data)');
     }
